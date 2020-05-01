0caa1ed4da7623c0eceeabee8edb864af7092453
CaAngeli <carla.bhering@gmail.com>
2020-05-01 17:22:46 -0600

    Base DroidCafe app - Tutorial 4.2 Challenge 2

--- app/src/main/java/com/sampleapps/carla/droidcafe/OrderActivity.java
+++ app/src/main/java/com/sampleapps/carla/droidcafe/OrderActivity.java
@@ -1,9 +1,14 @@
 package com.sampleapps.carla.droidcafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,6 +18,7 @@ import static com.sampleapps.carla.droidcafe.R.id.radioGroup;
 
 public class OrderActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private String orderMessage = new String();
 
     @Override
@@ -25,6 +31,21 @@ public class OrderActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(orderMessage);
 
+        EditText phoneText = findViewById(R.id.phone_text);
+        if (phoneText != null)
+            phoneText.setOnEditorActionListener
+                    (new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                            boolean handled = false;
+                            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                                dialNumber();
+                                handled = true;
+                            }
+                            return handled;
+                        }
+                        // If view is found, set the listener for editText.
+                    });
     }
 
     public void displayToast(String message) {
@@ -63,6 +84,29 @@ public class OrderActivity extends AppCompatActivity {
             default:
                 // Do nothing.
                 break;
+        }
+    }
+
+    private void dialNumber() {
+        // Find the editText_main view.
+        EditText editText = findViewById(R.id.phone_text);
+        String phoneNum = null;
+        // If the editText field is not null,
+        // concatenate "tel: " with the phone number string.
+        if (editText != null) phoneNum = "tel:" +
+                editText.getText().toString();
+        // Optional: Log the concatenated phone number for dialing.
+        Log.d(TAG, "dialNumber: " + phoneNum);
+        // Specify the intent.
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        intent.setData(Uri.parse(phoneNum));
+        // If the intent resolves to a package (app),
+        // start the activity with the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
         }
     }
 }
