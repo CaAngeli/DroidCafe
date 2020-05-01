914da51c552cf8a972c4638eeb4b24f4afc44bbd
CaAngeli <carla.bhering@gmail.com>
2020-05-01 16:41:43 -0600

    Base DroidCafe app - Tutorial 4.2 Task 3

--- app/src/main/java/com/sampleapps/carla/droidcafe/OrderActivity.java
+++ app/src/main/java/com/sampleapps/carla/droidcafe/OrderActivity.java
@@ -3,7 +3,13 @@ package com.sampleapps.carla.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.sampleapps.carla.droidcafe.R.id.radioGroup;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -18,5 +24,45 @@ public class OrderActivity extends AppCompatActivity {
         orderMessage = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(orderMessage);
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        RadioGroup radiogroup = findViewById(R.id.radioGroup);
+        radiogroup.check(R.id.sameday);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
     }
 }
