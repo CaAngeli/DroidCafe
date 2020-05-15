eaec3be5ec0f389994e5963ecd8d6dec03ef6227
CaAngeli <carla.bhering@gmail.com>
2020-05-15 15:38:23 -0600

    Base DroidCafe app - Tutorial 4.3 Task 3

--- app/src/main/java/com/sampleapps/carla/droidcafe/MainActivity.java
+++ app/src/main/java/com/sampleapps/carla/droidcafe/MainActivity.java
@@ -49,10 +49,24 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_order) {
-            return true;
+        switch (id) {
+            case R.id.action_order:
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // do nothing
         }
-
         return super.onOptionsItemSelected(item);
     }
 
